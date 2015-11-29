f615702c20d27ebe2de1ba47928820a6f72ef488
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 14:49:13 +0200

    support: utils: Introduce ScreenType class
    Change-Id: I6ddb14d6228565331d52069f4d96f93a9d306b45

---
+++ utilities/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_MODULE := cm-support-utilities
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_JAR_EXCLUDE_FILES := none
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
+    $(call all-Iaidl-files-under, src)
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
